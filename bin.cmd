7873443f465f8f7e70c4f95a0d4343e00b9f2d8f
zangchanglong <zangchanglong@camel4u.com>
2022-01-25 16:53:15 +0800

    Update start command...

--- bin.cmd
+++ bin.cmd
@@ -1,3 +1,3 @@
-set MAYA_LOCATION="C:/Program Files/Autodesk/Maya2018"
-"C:\Program Files\Autodesk\Maya2018\bin\mayapy.exe" exp_ui.py
+set MAYA_LOCATION=C:/Program Files/Autodesk/Maya2018
+"%MAYA_LOCATION%/bin/mayapy.exe" exp_ui.py
 pause
